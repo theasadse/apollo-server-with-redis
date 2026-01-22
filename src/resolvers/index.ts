75cbeeaf21ea491f395bfddf93dc9c38783588ff
theasadse <theasadse@gmail.com>
2026-01-22 12:17:54 +0500

    fixed issus

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -361,8 +361,6 @@ export const resolvers = {
       { title, content, authorId }: { title: string; content: string; authorId: string }
     ) {
       const newPost = await db.insert(posts).values({ title, content, authorId }).returning();
-      console.log("New Post:", newPost);
-      console.log("Author ID:", authorId);
       // Invalidate caches
       await cacheDel("posts:count");
       await cacheDel(`author:${authorId}:posts:count`);
